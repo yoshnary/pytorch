5d61ddd288de6a80f42c6e243bc6961d906da92e
Zafar Takhirov <cc.rafaz@zafar.cc>
2020-04-29 10:22:56 -0700

    [quant] quantized reflection_pad1d
    ghstack-source-id: 264196799f164198e11744ff3876723d1ca5ac6a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/37452

--- test/test_quantization.py
+++ test/test_quantization.py
@@ -11,6 +11,7 @@ from quantization.test_quantized_op import TestQNNPackOps  # noqa: F401
 from quantization.test_quantized_op import TestQuantizedLinear  # noqa: F401
 from quantization.test_quantized_op import TestDynamicQuantizedLinear  # noqa: F401
 from quantization.test_quantized_op import TestComparatorOps  # noqa: F401
+from quantization.test_quantized_op import TestPadding  # noqa: F401
 
 # Quantized Functional
 from quantization.test_quantized_functional import TestQuantizedFunctional  # noqa: F401
