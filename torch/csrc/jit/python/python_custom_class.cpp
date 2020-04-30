5bffcefa22a3aa396428d911d35f29e427a0bf53
Michael Suo <suo@fb.com>
2020-04-30 09:35:55 -0700

    Better error message when missing init.
    Previously we would segfault
    
    ghstack-source-id: 032ea6edaec6235cec51eaf2d084e44c34975c48
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/37474

--- torch/csrc/jit/python/python_custom_class.cpp
+++ torch/csrc/jit/python/python_custom_class.cpp
@@ -10,8 +10,16 @@ struct CustomObjectProxy;
 py::object ScriptClass::__call__(py::args args, py::kwargs kwargs) {
   auto instance =
       Object(at::ivalue::Object::create(class_type_, /*numSlots=*/1));
-  return invokeScriptMethodFromPython(
-      instance, "__init__", std::move(args), std::move(kwargs));
+  Function* init_fn = instance.type()->findMethod("__init__");
+  TORCH_CHECK(
+      init_fn,
+      "Custom C++ class: '",
+      instance.type()->python_str(),
+      "' does not have an '__init__' method bound. "
+      "Did you forget to add '.def(torch::init<...>)' to its registration?")
+  Method init_method(instance._ivalue(), init_fn);
+  invokeScriptMethodFromPython(init_method, std::move(args), std::move(kwargs));
+  return py::cast(instance);
 }
 
 void initPythonCustomClassBindings(PyObject* module) {
